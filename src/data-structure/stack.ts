59350cd1c0bd51ea9cf7b97477d99b27d035bd90
lc <innovgame@163.com>
2020-04-12 14:46:00 +0800

    Data Structure Stack

---
+++ src/data-structure/stack.ts
@@ -0,0 +1,60 @@
+interface Items<T> {
+    [index: number]: T;
+}
+
+export class Stack<T> implements IStack<T> {
+    protected count: number;
+    protected items: Items<T>;
+
+    constructor() {
+        this.count = 0;
+        this.items = Object.create(null);
+    }
+    push(val: T): void {
+        this.items[this.count++] = val;
+    }
+    pop(): T {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+        const result = this.items[--this.count];
+        delete this.items[this.count];
+        return result;
+    }
+    peek(): T {
+        if (this.isEmpty()) {
+            return undefined;
+        }
+        return this.items[this.count - 1];
+    }
+    isEmpty(): boolean {
+        return this.count === 0;
+    }
+    size(): number {
+        return this.count;
+    }
+    clear(): void {
+        this.items = Object.create(null);
+        this.count = 0;
+    }
+    toString(): string {
+        if (this.isEmpty()) {
+            return "";
+        }
+        let objString = `${this.items[0]}`;
+        for (let i = 1; i < this.count; i++) {
+            objString = `${objString},${this.items[i]}`;
+        }
+        return objString;
+    }
+}
+
+export interface IStack<T> {
+    push(val: T): void;
+    pop(): T;
+    peek(): T;
+    isEmpty(): boolean;
+    size(): number;
+    clear(): void;
+    toString(): string;
+}
--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,19 @@
-import { test } from "./test";
-
 import "./style.css";
+import { Stack } from "./data-structure/index";
 
 if (module.hot) {
-    module.hot.accept("./test.ts", function () {
+    module.hot.accept("./data-structure/stack", function () {
         console.log("Accepting the updated test module!");
-        test();
     });
 }
 
-console.log("init...");
-const title = document.createElement("h2");
-title.innerHTML = "Data Structures";
-title.className = "hello";
-document.body.append(title);
-test();
+const stack = new Stack<string>();
+
+stack.push("111");
+stack.push("222");
+stack.push("333");
+
+console.log(stack.peek());
+console.log(stack.pop());
+
+console.log(stack);
